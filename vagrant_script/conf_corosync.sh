6515ade7a8286bc46ad61804a4da7a5a388aa5b0
Peter Lemenkov <lemenkov@redhat.com>
2016-05-04 16:58:52 +0200

    Use fedora-specific setup
    Signed-off-by: Peter Lemenkov <lemenkov@redhat.com>

--- vagrant_script/conf_corosync.sh
+++ vagrant_script/conf_corosync.sh
@@ -3,21 +3,37 @@
 # Protect from an incident running on hosts which aren't n1, n2, etc.
 ! [[ `hostname` =~ ^n[0-9]+$ ]] && exit 1
 
-IP=`ip addr show | grep -E '^[ ]*inet' | grep -m1 global | awk '{ print $2 }' | sed -e 's/\/.*//'`
-sed -i "s/bindnetaddr: 127.0.0.1/bindnetaddr: $IP/g" /etc/corosync/corosync.conf
-if ! timeout --signal=KILL 30 service corosync restart
-then
-  pkill -f -9 corosync
-  service corosync start
-fi
-if ! timeout --signal=KILL 30 service pacemaker restart
-then
-  pkill -f -9 pacemaker
-  service pacemaker start
-fi
-# FIXME(bogdando) A w/a to ensure the pacemakerd is respawning, if failed
-cp -f /vagrant/conf/pacemakerd.service /lib/systemd/system || /bin/true
-systemctl daemon-reload || /bin/true
-systemctl enable pacemakerd || /bin/true
-systemctl start pacemakerd || /bin/true
+OSTYPE=`grep -o -P "(?<=^ID\=).*$" /etc/os-release 2>&1`
+
+case "$OSTYPE" in
+	fedora)
+		dnf install -y corosync pacemaker pcs fence-agents-all fence-virt resource-agents
+		#cp -arv /etc/corosync/corosync.conf.example /etc/corosync/corosync.conf
+		echo -n "testpass" | passwd hacluster --stdin
+		systemctl start pcsd.service
+		systemctl enable pcsd.service
+		;;
+	ubuntu)
+		IP=`ip addr show | grep -E '^[ ]*inet' | grep -m1 global | awk '{ print $2 }' | sed -e 's/\/.*//'`
+		sed -i "s/bindnetaddr: 127.0.0.1/bindnetaddr: $IP/g" /etc/corosync/corosync.conf
+		if ! timeout --signal=KILL 30 service corosync restart
+		then
+		  pkill -f -9 corosync
+		  service corosync start
+		fi
+		if ! timeout --signal=KILL 30 service pacemaker restart
+		then
+		  pkill -f -9 pacemaker
+		  service pacemaker start
+		fi
+		# FIXME(bogdando) A w/a to ensure the pacemakerd is respawning, if failed
+		cp -f /vagrant/conf/pacemakerd.service /lib/systemd/system || /bin/true
+		systemctl daemon-reload || /bin/true
+		systemctl enable pacemakerd || /bin/true
+		systemctl start pacemakerd || /bin/true
+		;;
+	*)
+		;;
+esac
+
 exit 0
--- vagrant_script/conf_rabbit_ocf.sh
+++ vagrant_script/conf_rabbit_ocf.sh
@@ -1,14 +1,35 @@
 #!/bin/sh
-if [ "${UPLOAD_METHOD}" = "copy" ] ; then
-  [ "${OCF_RA_PATH}" ] || exit 1
-  echo "Copy the rabbit OCF RA from ${OCF_RA_PATH}"
-  cp -f "${OCF_RA_PATH}" /tmp/rabbitmq-server-ha
-elif [ "${UPLOAD_METHOD}" = "none" ] ; then
-  echo "Do not upload the rabbit OCF RA"
-else
-  echo "Download the rabbit OCF RA from the stable branch"
-  wget https://raw.githubusercontent.com/rabbitmq/rabbitmq-server/stable/scripts/rabbitmq-server-ha.ocf -O /tmp/rabbitmq-server-ha
-fi
-chmod +x /tmp/rabbitmq-server-ha
-cp -f /tmp/rabbitmq-server-ha /usr/lib/ocf/resource.d/rabbitmq/
+
+OSTYPE=`grep -o -P "(?<=^ID\=).*$" /etc/os-release 2>&1`
+
+case "$OSTYPE" in
+	fedora)
+		# fedora
+		dnf install -y corosync pacemaker pcs fence-agents-all fence-virt resource-agents
+		#cp -arv /etc/corosync/corosync.conf.example /etc/corosync/corosync.conf
+		echo -n "testpass" | passwd hacluster --stdin
+		systemctl start pcsd.service
+		systemctl enable pcsd.service
+
+		cp -f /usr/lib/ocf/resource.d/heartbeat/rabbitmq-cluster /root/rabbitmq-cluster.orig
+
+		curl https://raw.githubusercontent.com/lemenkov/resource-agents/cce72dc42346e6fd1fa3dd0ae77911ee256ab1e1/heartbeat/rabbitmq-cluster -o /tmp/rabbitmq-cluster
+		cp -f /tmp/rabbitmq-cluster /usr/lib/ocf/resource.d/heartbeat/rabbitmq-cluster
+		;;
+	ubuntu)
+		if [ "${UPLOAD_METHOD}" = "copy" ] ; then
+		  [ "${OCF_RA_PATH}" ] || exit 1
+		  echo "Copy the rabbit OCF RA from ${OCF_RA_PATH}"
+		  cp -f "${OCF_RA_PATH}" /tmp/rabbitmq-server-ha
+		elif [ "${UPLOAD_METHOD}" = "none" ] ; then
+		  echo "Do not upload the rabbit OCF RA"
+		else
+		  echo "Download the rabbit OCF RA from the stable branch"
+		  wget https://raw.githubusercontent.com/rabbitmq/rabbitmq-server/stable/scripts/rabbitmq-server-ha.ocf -O /tmp/rabbitmq-server-ha
+		fi
+		chmod +x /tmp/rabbitmq-server-ha
+		cp -f /tmp/rabbitmq-server-ha /usr/lib/ocf/resource.d/rabbitmq/
+		;;
+esac
+
 exit 0
